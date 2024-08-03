1677648ab535c74b93496174255282b3dada687b
qs_theme <qstxdy@gmail.com>
2024-08-03 17:29:06 +0800

    add new game mathKiller

--- frontEnd/src/views/game.vue
+++ frontEnd/src/views/game.vue
@@ -1,10 +1,19 @@
 <template>
     <div class="postLink">
-        <a href="https://game.watering.top/singleThreeChess.html">
+        <a href="https://game.watering.top/threeChess/singleThreeChess.html">
             <div>
                 <h2>井字棋单人版</h2>
                 <p>朴实无华的井字棋</p>
                 <div class="time"><span>2024-7-26 10:53</span></div>
+            </div>
+        </a>
+    </div>
+    <div class="postLink">
+        <a href="https://game.watering.top/mathKiller/mathKiller.html">
+            <div>
+                <h2>点击图形</h2>
+                <p>类似水果忍者？不过做的很简陋，一直懒得改进。</p>
+                <div class="time"><span>2024-8-3 17:27</span></div>
             </div>
         </a>
     </div>
